e9382c7b406004e2cb32325401a4bf8562e5df11
Daniel <cariasdaniel261@gmail.com>
2023-09-04 15:23:07 -0600

    feat(Default-Directory-Projects): Added function to create the default projects directory if it doesn't exist

--- src/jvmMain/kotlin/ui/Main.kt
+++ src/jvmMain/kotlin/ui/Main.kt
@@ -1,5 +1,6 @@
 package ui
 
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
@@ -7,10 +8,13 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
+import kotlinx.coroutines.launch
+import util.DocumentsManager
 import java.awt.Toolkit
 
 fun main() = application {
     val toolkit = Toolkit.getDefaultToolkit().screenSize
+    rememberCoroutineScope().launch { DocumentsManager.createDefaultProjectsDirectory() }
     Window(
         onCloseRequest = ::exitApplication,
         title = "DeepCode Studio",
--- src/jvmMain/kotlin/util/DocumentsManager.kt
+++ src/jvmMain/kotlin/util/DocumentsManager.kt
@@ -1,5 +1,7 @@
 package util
 
+import java.io.File
+
 object DocumentsManager {
     /**
      * Retrieve the path of the user's home directory
@@ -7,4 +9,15 @@ object DocumentsManager {
      * @return The path of the user's home directory
      */
     fun getUserHome(): String = System.getProperty("user.home")
+
+    /**
+     * Creates the default projects directory in the user's home directory if it doesn't exist
+     */
+    fun createDefaultProjectsDirectory(){
+        val projectsDirectory = File("${getUserHome()}/${Constants.DEFAULT_PROJECTS_DIRECTORY_NAME}")
+        if(!projectsDirectory.exists()){
+            kotlin.runCatching { projectsDirectory.mkdir() }
+                .onFailure { exception -> println("An error occurred while creating the projects directory: ${exception.message}") }
+        }
+    }
 }
