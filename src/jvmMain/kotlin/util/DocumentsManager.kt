2bebe722a1ad980b079ef5976631c2fa002f4ca7
Daniel <cariasdaniel261@gmail.com>
2023-09-11 20:41:38 -0600

    feat(Auto-Save): Added automatic code writing to file function

--- src/jvmMain/kotlin/util/DocumentsManager.kt
+++ src/jvmMain/kotlin/util/DocumentsManager.kt
@@ -20,4 +20,23 @@ object DocumentsManager {
                 .onFailure { exception -> println("An error occurred while creating the projects directory: ${exception.message}") }
         }
     }
+
+    /**
+     * Writes the given content to a file
+     *
+     * @param file The File object representing the file to write to
+     * @param content The content to be written to the file
+     * @return `true` if the writing was successful, `false` otherwise
+     */
+    fun writeFile(file: File, content: String): Boolean?{
+        return runCatching {
+            file.bufferedWriter().use { writer -> writer.write(content)}
+            true
+        }
+            .onFailure { exception ->
+                println("An error occurred while writing to the file: ${exception.message}")
+                return false
+            }
+            .getOrNull()
+    }
 }
