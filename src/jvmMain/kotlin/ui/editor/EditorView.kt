472ed0ba2f1a06879989a33b7ffc6b982f08a7a6
Daniel <cariasdaniel261@gmail.com>
2023-10-13 14:59:37 -0600

    feat(Welcome-Screen): Added welcome screen

--- src/jvmMain/kotlin/ui/editor/EditorView.kt
+++ src/jvmMain/kotlin/ui/editor/EditorView.kt
@@ -1,19 +1,17 @@
 package ui.editor
 
-import androidx.compose.foundation.ScrollbarAdapter
-import androidx.compose.foundation.VerticalScrollbar
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.input.key.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -48,6 +46,13 @@ fun EditorView(
     // Coroutine scope for handling coroutines within the Composable
     val coroutineScope = rememberCoroutineScope()
 
+    var showEditor by remember { mutableStateOf(false) }
+
+    LaunchedEffect(tabsState.tabs.size){
+        // If the tab size is different from 0, the editor is displayed; otherwise, the welcome screen is shown
+        showEditor = tabsState.tabs.size != 0
+    }
+
     Column(modifier = Modifier.fillMaxSize()) {
 
         // Write the editor content to the associated file
@@ -60,136 +65,160 @@ fun EditorView(
 
         Spacer(modifier = Modifier.height(5.dp))
 
-        Box(modifier = Modifier.fillMaxSize()){
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(scrollState)
-            ) {
-                Row{
+        // Here
+        if(showEditor){
+            Box(modifier = Modifier.fillMaxSize()){
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(scrollState)
+                ) {
+                    Row{
 
-                    LinesNumberView(editorState.linesCount.value)
+                        LinesNumberView(editorState.linesCount.value)
 
-                    Spacer(modifier = Modifier.width(10.dp))
+                        Spacer(modifier = Modifier.width(10.dp))
 
-                    BasicTextField(
-                        value = editorState.codeText.value,
-                        onValueChange = {
-                            if (it.text != editorState.codeText.value.text){
-                                val selectedWord = TextUtils.extractSurroundingWord(it.selection.start, it.text)
-                                editorState.wordToSearch.value = selectedWord
+                        BasicTextField(
+                            value = editorState.codeText.value,
+                            onValueChange = {
+                                if (it.text != editorState.codeText.value.text){
+                                    val selectedWord = TextUtils.extractSurroundingWord(it.selection.start, it.text)
+                                    editorState.wordToSearch.value = selectedWord
 
-                                editorState.autoCompleteSuggestions.value = KeywordAutoCompleteUtil.autoCompleteKeywords(selectedWord) +
-                                        KeywordAutoCompleteUtil.filterVariableNamesForAutocomplete(TextUtils.extractVariableNames(editorState.codeText.value.text), selectedWord)
+                                    editorState.autoCompleteSuggestions.value = KeywordAutoCompleteUtil.autoCompleteKeywords(selectedWord) +
+                                            KeywordAutoCompleteUtil.filterVariableNamesForAutocomplete(TextUtils.extractVariableNames(editorState.codeText.value.text), selectedWord)
 
-                                editorState.isAutoCompleteVisible.value = editorState.autoCompleteSuggestions.value.isNotEmpty()
-                            } else {
-                                editorState.autoCompleteSuggestions.value = emptyList()
-                                editorState.isAutoCompleteVisible.value = false
-                            }
-
-                            editorState.codeText.value = it
-
-                        },
-                        onTextLayout = {
-                            val lineCount = it.lineCount
-                            if(lineCount != editorState.linesCount.value) editorState.linesCount.value = lineCount
-                            editorState.textLayoutResult.value = it
-                            editorState.cursorXCoordinate.value = it.getHorizontalPosition(editorState.codeText.value.selection.start, true).toInt()
-                        },
-                        textStyle = TextStyle(
-                            fontSize = 13.sp,
-                            color = ThemeApp.colors.textColor,
-                            fontFamily = ThemeApp.text.codeTextFontFamily,
-                            fontWeight = FontWeight.W500
-                        ),
-                        cursorBrush = SolidColor(ThemeApp.colors.buttonColor),
-                        visualTransformation = EditorVisualTransformation(),
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .focusRequester(editorState.textFieldFocusRequester.value)
-                            .onPreviewKeyEvent {
-                                if(it.key == Key.DirectionDown && editorState.isAutoCompleteVisible.value && !editorState.isKeyBeingPressed.value){
-                                    if(editorState.selectedItemIndex.value < editorState.autoCompleteSuggestions.value.size -1 ) editorState.selectedItemIndex.value ++
-                                    editorState.isKeyBeingPressed.value = true
-                                    true
-                                } else if(it.key == Key.DirectionUp && editorState.isAutoCompleteVisible.value && !editorState.isKeyBeingPressed.value){
-                                    if(editorState.selectedItemIndex.value > 0) editorState.selectedItemIndex.value --
-                                    editorState.isKeyBeingPressed.value = true
-                                    true
-                                } else if(it.key == Key.Enter && editorState.isAutoCompleteVisible.value && !editorState.isKeyBeingPressed.value){
-                                    val newText = TextUtils.insertTextAtCursorPosition(editorState.codeText.value.selection.start, editorState.codeText.value.text, editorState.autoCompleteSuggestions.value[editorState.selectedItemIndex.value])
-                                    editorState.codeText.value = TextFieldValue(newText, TextRange(editorState.codeText.value.selection.start + editorState.autoCompleteSuggestions.value[editorState.selectedItemIndex.value].length - editorState.wordToSearch.value.length))
-                                    editorState.selectedItemIndex.value = 0
+                                    editorState.isAutoCompleteVisible.value = editorState.autoCompleteSuggestions.value.isNotEmpty()
+                                } else {
                                     editorState.autoCompleteSuggestions.value = emptyList()
                                     editorState.isAutoCompleteVisible.value = false
-                                    editorState.isKeyBeingPressed.value = true
-                                    true
-                                } else if(it.key == Key.Enter && !editorState.isKeyBeingPressed.value){
-                                    coroutineScope.launch{ scrollState.scrollTo(scrollState.value + 10) }
-
-                                    // Extract the current code text, cursor position, and text before the cursor
-                                    val codeText = editorState.codeText.value.text
-                                    val cursorPosition = editorState.codeText.value.selection.start
-                                    val textBeforeCursor = codeText.substring(0, cursorPosition)
-
-                                    // Extract the last line of text and split the text before the cursor into individual words
-                                    val lastLine = textBeforeCursor.lines().last()
-                                    val wordsInText = textBeforeCursor.split("\\s+".toRegex())
-
-                                    // Check if specific conditions are met to determine whether to add spaces and newlines
-                                    if(codeText.isNotEmpty() &&
-                                        textBeforeCursor.last() == ':' ||
-                                        (lastLine.contains("   ") && lastLine != "   ") ||
-                                        wordsInText.last() == ".data" ||
-                                        wordsInText.last() == ".bss" ||
-                                        wordsInText.last() == ".text"
-                                    ) {
-                                        editorState.codeText.value = TextUtils.insertSpacesInText(codeText, cursorPosition, "\n   ")
-                                    } else {
-                                        editorState.codeText.value = TextUtils.insertSpacesInText(codeText, cursorPosition, "\n")
-                                    }
-
-                                    editorState.isKeyBeingPressed.value = true
-                                    true
-                                } else if(it.key == Key.Tab && !editorState.isKeyBeingPressed.value){
-                                    editorState.codeText.value = TextUtils.insertSpacesInText(editorState.codeText.value.text, editorState.codeText.value.selection.start, "   ")
-                                    editorState.isKeyBeingPressed.value = true
-                                    true
-                                } else if(it.type == KeyEventType.KeyUp){
-                                    editorState.isKeyBeingPressed.value = false
-                                    false
-                                } else {
-                                    false
                                 }
-                            }
-                    )
+
+                                editorState.codeText.value = it
+
+                            },
+                            onTextLayout = {
+                                val lineCount = it.lineCount
+                                if(lineCount != editorState.linesCount.value) editorState.linesCount.value = lineCount
+                                editorState.textLayoutResult.value = it
+                                editorState.cursorXCoordinate.value = it.getHorizontalPosition(editorState.codeText.value.selection.start, true).toInt()
+                            },
+                            textStyle = TextStyle(
+                                fontSize = 13.sp,
+                                color = ThemeApp.colors.textColor,
+                                fontFamily = ThemeApp.text.codeTextFontFamily,
+                                fontWeight = FontWeight.W500
+                            ),
+                            cursorBrush = SolidColor(ThemeApp.colors.buttonColor),
+                            visualTransformation = EditorVisualTransformation(),
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .focusRequester(editorState.textFieldFocusRequester.value)
+                                .onPreviewKeyEvent {
+                                    if(it.key == Key.DirectionDown && editorState.isAutoCompleteVisible.value && !editorState.isKeyBeingPressed.value){
+                                        if(editorState.selectedItemIndex.value < editorState.autoCompleteSuggestions.value.size -1 ) editorState.selectedItemIndex.value ++
+                                        editorState.isKeyBeingPressed.value = true
+                                        true
+                                    } else if(it.key == Key.DirectionUp && editorState.isAutoCompleteVisible.value && !editorState.isKeyBeingPressed.value){
+                                        if(editorState.selectedItemIndex.value > 0) editorState.selectedItemIndex.value --
+                                        editorState.isKeyBeingPressed.value = true
+                                        true
+                                    } else if(it.key == Key.Enter && editorState.isAutoCompleteVisible.value && !editorState.isKeyBeingPressed.value){
+                                        val newText = TextUtils.insertTextAtCursorPosition(editorState.codeText.value.selection.start, editorState.codeText.value.text, editorState.autoCompleteSuggestions.value[editorState.selectedItemIndex.value])
+                                        editorState.codeText.value = TextFieldValue(newText, TextRange(editorState.codeText.value.selection.start + editorState.autoCompleteSuggestions.value[editorState.selectedItemIndex.value].length - editorState.wordToSearch.value.length))
+                                        editorState.selectedItemIndex.value = 0
+                                        editorState.autoCompleteSuggestions.value = emptyList()
+                                        editorState.isAutoCompleteVisible.value = false
+                                        editorState.isKeyBeingPressed.value = true
+                                        true
+                                    } else if(it.key == Key.Enter && !editorState.isKeyBeingPressed.value){
+                                        coroutineScope.launch{ scrollState.scrollTo(scrollState.value + 10) }
+
+                                        // Extract the current code text, cursor position, and text before the cursor
+                                        val codeText = editorState.codeText.value.text
+                                        val cursorPosition = editorState.codeText.value.selection.start
+                                        val textBeforeCursor = codeText.substring(0, cursorPosition)
+
+                                        // Extract the last line of text and split the text before the cursor into individual words
+                                        val lastLine = textBeforeCursor.lines().last()
+                                        val wordsInText = textBeforeCursor.split("\\s+".toRegex())
+
+                                        // Check if specific conditions are met to determine whether to add spaces and newlines
+                                        if(codeText.isNotEmpty() &&
+                                            textBeforeCursor.last() == ':' ||
+                                            (lastLine.contains("   ") && lastLine != "   ") ||
+                                            wordsInText.last() == ".data" ||
+                                            wordsInText.last() == ".bss" ||
+                                            wordsInText.last() == ".text"
+                                        ) {
+                                            editorState.codeText.value = TextUtils.insertSpacesInText(codeText, cursorPosition, "\n   ")
+                                        } else {
+                                            editorState.codeText.value = TextUtils.insertSpacesInText(codeText, cursorPosition, "\n")
+                                        }
+
+                                        editorState.isKeyBeingPressed.value = true
+                                        true
+                                    } else if(it.key == Key.Tab && !editorState.isKeyBeingPressed.value){
+                                        editorState.codeText.value = TextUtils.insertSpacesInText(editorState.codeText.value.text, editorState.codeText.value.selection.start, "   ")
+                                        editorState.isKeyBeingPressed.value = true
+                                        true
+                                    } else if(it.type == KeyEventType.KeyUp){
+                                        editorState.isKeyBeingPressed.value = false
+                                        false
+                                    } else {
+                                        false
+                                    }
+                                }
+                        )
+                    }
+
+                    Box(modifier = Modifier.fillMaxWidth().height(200.dp))
+
                 }
 
-                Box(modifier = Modifier.fillMaxWidth().height(200.dp))
+                // Display teh auto-complete dropdown if suggestions are available
+                if(editorState.isAutoCompleteVisible.value){
+                    AutoCompleteDropdown(
+                        items = editorState.autoCompleteSuggestions.value,
+                        editorState = editorState,
+                        cursorX = editorState.cursorXCoordinate.value,
+                        cursorY = editorState.cursorYCoordinate.value - scrollState.value,
+                        selectedItemIndex = editorState.selectedItemIndex.value
+                    )
+                } else editorState.selectedItemIndex.value = 0
+
+                // Vertical scroll for the code editor
+                VerticalScrollbar(
+                    ScrollbarAdapter(scrollState),
+                    modifier = Modifier
+                        .align(Alignment.CenterEnd)
+                        .fillMaxHeight(),
+                    style = ThemeApp.scrollbar.scrollbarStyle
+                )
 
             }
-
-            // Display teh auto-complete dropdown if suggestions are available
-            if(editorState.isAutoCompleteVisible.value){
-                AutoCompleteDropdown(
-                    items = editorState.autoCompleteSuggestions.value,
-                    editorState = editorState,
-                    cursorX = editorState.cursorXCoordinate.value,
-                    cursorY = editorState.cursorYCoordinate.value - scrollState.value,
-                    selectedItemIndex = editorState.selectedItemIndex.value
+        } else {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ){
+                Image(
+                    painterResource("images/ic_app.svg"),
+                    contentDescription = "App icon",
+                    modifier = Modifier.size(300.dp)
                 )
-            } else editorState.selectedItemIndex.value = 0
 
-            // Vertical scroll for the code editor
-            VerticalScrollbar(
-                ScrollbarAdapter(scrollState),
-                modifier = Modifier
-                    .align(Alignment.CenterEnd)
-                    .fillMaxHeight(),
-                style = ThemeApp.scrollbar.scrollbarStyle
-            )
+                Text(
+                    "DeepCode Studio",
+                    color = ThemeApp.colors.secondColor,
+                    fontFamily = ThemeApp.text.fontFamily,
+                    fontSize = 25.sp,
+                    fontWeight = FontWeight.ExtraBold
+                )
 
+            }
         }
     }
 }
--- src/jvmMain/kotlin/ui/editor/LinesNumberView.kt
+++ src/jvmMain/kotlin/ui/editor/LinesNumberView.kt
@@ -1,8 +1,6 @@
 package ui.editor
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
